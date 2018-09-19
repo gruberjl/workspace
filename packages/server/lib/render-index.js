453c37bd23f78652fb48fc1bb204f16ee5d92645
John Gruber <john.gruber@gitbit.org>
2018-09-19 12:38:34 -0400

    sorted messages

--- packages/server/lib/render-index.js
+++ packages/server/lib/render-index.js
@@ -1,10 +1,10 @@
 const {db} = require('../../firestore')
 
 const renderIndex = async (req, res) => {
-  const snapshot = await db.collection('messages').get()
+  const snapshot = await db.collection('messages').orderBy('created', 'desc').get()
   const messages = []
   snapshot.forEach(doc => messages.push(doc.data()))
-  res.render('index', {flash: res.flash, messages: messages.reverse()})
+  res.render('index', {flash: res.flash, messages})
 }
 
 module.exports = {renderIndex}
