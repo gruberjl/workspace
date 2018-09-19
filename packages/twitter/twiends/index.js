b9ae4b638b7fda1fb0c8243c7168e2110213258e
John Gruber <john.gruber@gitbit.org>
2018-09-19 13:15:49 -0400

    added twiends

--- packages/twitter/twiends/index.js
+++ packages/twitter/twiends/index.js
@@ -1,9 +1,29 @@
 const {login} = require('./lib/login')
 const {follow} = require('./lib/follow')
+const {db} = require('../../firestore')
 
-const start = async (username, password) => {
-  if (!username || !password) throw ('username & password required')
+let nextRun
 
+const shouldRun = (twiendsDoc) => {
+  if (!nextRun) nextRun = (twiendsDoc.data().lastRun) + 60 * 60 * 24 * 1000
+
+  const time = new Date().getTime()
+  if (nextRun > time) return false
+
+  nextRun = time + 60 * 60 * 24 * 1000
+
+  return true
+}
+
+const start = async () => {
+  const twiendsDoc = await db.collection('common').doc('twiends').get()
+  if (!shouldRun(twiendsDoc)) return
+
+  await db.collection('common').doc('twiends').set(
+    Object.assign({}, twiendsDoc.data(), {lastRun:(new Date().getTime())})
+  )
+
+  const {username, password} = twiendsDoc.data()
   const driver = await login(username, password)
   await follow(driver)
   await driver.quit()
