0cee67d7d0a6ce2170e26a001a871b9133075435
John Gruber <john.gruber@gitbit.org>
2018-10-01 11:01:28 -0400

    updated get-cookies to add all cookies

--- packages/driver/get-cookies.js
+++ packages/driver/get-cookies.js
@@ -1,12 +1,21 @@
+const providers = ['scoopIt', 'flipboard', 'medium', 'digg', 'mix', 'stumbleupon', 'reddit', 'twitter', 'facebook', 'bizsugar', 'pinterest', 'linkedin', 'tumblr']
+
+const addCookies = async (driver, cookies) => {
+  for (let i = 0; i < cookies.length; i++) {
+    await driver.manage().addCookie(cookies[i])
+  }
+}
+
 const getCookies = async (driver, personDoc) => {
   await driver.get('http://gitbit.org')
 
   const cookies = personDoc.data().cookies
-  if (!cookies) return
+  if (cookies) await addCookies(driver, cookies)
 
-  for (let i = 0; i < cookies.length; i++) {
-    await driver.manage().addCookie(cookies[i])
-  }
+  providers.forEach(providerKey => {
+    const provider = personDoc.data()[providerKey]
+    if (provider.cookies) addCookies(driver, provider.cookies)
+  })
 }
 
 module.exports = {getCookies}
