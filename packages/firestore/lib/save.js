d3a7b302ae341c125103d41550ac1988a21b373a
John Gruber <john.gruber@gitbit.org>
2018-10-02 21:27:42 -0400

    created save

--- packages/firestore/lib/save.js
+++ packages/firestore/lib/save.js
@@ -1,5 +1,9 @@
-const save = async (doc, data) => {
+const merge = require('deepmerge')
 
+const save = async (doc, data) => {
+  const latestDoc = await doc.ref.get()
+  const merged = merge(latestDoc.data(), data)
+  await latestDoc.ref.set(merged)
 }
 
 module.exports = save
